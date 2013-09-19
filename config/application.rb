a1792a57967ca437df4042611dbb1874a1d74af3
andrewsotnikov <a.sotnikov@libertyrus.ru>
2013-09-19 11:29:13 +0400

    set utf-8

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ Bundler.require(:default, Rails.env)
 
 module SampleApp
   class Application < Rails::Application
-    config.encoding = 'windows-1251'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
