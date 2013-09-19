fd24723652e43fdaa7eae37ffab2ef5ce53807df
andrewsotnikov <a.sotnikov@libertyrus.ru>
2013-09-19 11:24:14 +0400

    charset 1251

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(:default, Rails.env)
 
 module SampleApp
   class Application < Rails::Application
-    # config.encoding = 'windows-1251'
+    config.encoding = 'windows-1251'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
